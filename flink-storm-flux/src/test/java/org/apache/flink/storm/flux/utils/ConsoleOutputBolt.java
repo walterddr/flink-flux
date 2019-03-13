eb65fd9ca7fb2e2a3a29f77740cada4dc3531371
Rong Rong <walter_ddr@hotmail.com>
2019-03-12 18:39:44 -0700

    modify to use console output for E2E output. will change later for kafka in kafka out testing

---
+++ flink-storm-flux/src/test/java/org/apache/flink/storm/flux/utils/ConsoleOutputBolt.java
@@ -0,0 +1,19 @@
+package org.apache.flink.storm.flux.utils;
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class ConsoleOutputBolt extends BaseBasicBolt {
+
+  public ConsoleOutputBolt() {
+  }
+
+  public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
+    System.out.println(tuple);
+  }
+
+  public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+  }
+}
